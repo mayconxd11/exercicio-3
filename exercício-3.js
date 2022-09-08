ec97f83fe08a595ad98b4e2e049b1b78b32ef5bd
unknown <mayconxd11>
2022-09-08 17:20:40 -0300

    exercicios-3

---
+++ exercício-3.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')()
+
+const peso = prompt('Seu peso:');
+console.log('\n')
+
+const nome = prompt('Seu nome:');
+console.log('\n')
+
+const altura = prompt('Sua altura:')
+console.log ('\n')
+
+const dataDeInternaçao = prompt ('Data que da sua Internação:')
+console.log =('\n')
+
+const diabetico = prompt('Você é diabético ou não:')
+console.log('\n')
